031aef4f4bbfe15813e325a7e83b78dacc662f02
Roman Lapin <lampus.lapin@gmail.com>
2011-10-27 16:58:16 +0400

    Added dummy story/show functions for pll1_1

--- cdce913.c
+++ cdce913.c
@@ -254,13 +254,43 @@ static ssize_t cdce913_store_pll1_0(struct device *dev,
 	return count;
 }
 
+static ssize_t cdce913_show_pll1_1(struct device *dev,
+				struct device_attribute *attr,
+                char *buf)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	struct cdce913_pll *dev_data = i2c_get_clientdata(client);
+
+	return scnprintf(buf, PAGE_SIZE, "0x%08X\n", dev_data->pll[1].data);
+}
+
+static ssize_t cdce913_store_pll1_1(struct device *dev,
+				 struct device_attribute *attr,
+				 const char *buf, size_t count)
+{
+	u8 i;
+	unsigned long tmp;
+	struct i2c_client *client = to_i2c_client(dev);
+	struct cdce913_pll *dev_data = i2c_get_clientdata(client); 
+	
+	if (strict_strtoul(buf, 16, &tmp) < 0)
+		return -EINVAL;
+		
+	mutex_lock(&dev_data->lock);
+	dev_data->pll[1].data = tmp;
+	for(i = 0; i < 4; i++)
+		cdce913_write(client, CDCE913_REG(PLL1_1N_11_4) + i, dev_data->pll[1].darr[3-i]);
+	mutex_unlock(&dev_data->lock);
+	return count;
+}
+
 static DEVICE_ATTR(pdiv, S_IWUSR|S_IRUSR, cdce913_show_pdiv, cdce913_store_pdiv);
 static DEVICE_ATTR(y1, S_IWUSR|S_IRUSR, cdce913_show_y1, cdce913_store_y1);
 static DEVICE_ATTR(y2y3, S_IWUSR|S_IRUSR, cdce913_show_y2y3, cdce913_store_y2y3);
 static DEVICE_ATTR(fs1, S_IWUSR|S_IRUSR, cdce913_show_fs1, cdce913_store_fs1);
 static DEVICE_ATTR(pll1_0, S_IWUSR|S_IRUSR, cdce913_show_pll1_0, cdce913_store_pll1_0);
-/*
 static DEVICE_ATTR(pll1_1, S_IWUSR|S_IRUSR, cdce913_show_pll1_1, cdce913_store_pll1_1);
+/*
 static DEVICE_ATTR(ssc1, S_IWUSR|S_IRUSR, cdce913_show_ssc1, cdce913_store_ssc1);
 */
 static struct attribute *cdce913_attributes[] = {
@@ -269,6 +299,7 @@ static struct attribute *cdce913_attributes[] = {
 	&dev_attr_y2y3.attr,
 	&dev_attr_fs1.attr,
 	&dev_attr_pll1_0.attr,
+	&dev_attr_pll1_1.attr,
 	NULL
 };
 
