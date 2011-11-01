8f7c6b30c4c19ac76417d263f1c3da91914e7f61
Roman Lapin <lampus.lapin@gmail.com>
2011-11-01 17:56:29 +0400

    Added store/show function for ssc1

--- cdce913.c
+++ cdce913.c
@@ -36,6 +36,7 @@ struct cdce913_pll {
 	u8 y1;
 	u8 y2y3;
 	u8 fs1;
+	u8 ssc1[8];
 	union pll_conf pll[2];
 };
 
@@ -280,15 +281,88 @@ static ssize_t cdce913_store_pll1_1(struct device *dev,
 	return count;
 }
 
+static ssize_t cdce913_show_ssc1(struct device *dev,
+				struct device_attribute *attr,
+                char *buf)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	struct cdce913_pll *dev_data = i2c_get_clientdata(client);
+	u8 i;
+	ssize_t ret_sz;
+	
+	scnprintf(buf, PAGE_SIZE, "0x%02X", dev_data->ssc1[0]);
+	for(i = 1; i < 8; i++) {
+		scnprintf(buf, PAGE_SIZE, "%s,0x%02X", buf, dev_data->ssc1[i]);
+	}
+	scnprintf(buf, PAGE_SIZE, "%s\n", buf);
+	return ret_sz;
+}
+
+static ssize_t cdce913_store_ssc1(struct device *dev,
+				 struct device_attribute *attr,
+				 const char *buf, size_t count)
+{
+	unsigned long tmp;
+	struct i2c_client *client = to_i2c_client(dev);
+	struct cdce913_pll *dev_data = i2c_get_clientdata(client); 
+	u8 ssc_num;
+	u8 ssc_value;
+	if (strict_strtoul(buf, 16, &tmp) < 0)
+		return -EINVAL;
+	// ! (pdiv_num > 3)
+	// ! (pdiv_value > 1023)
+	if(tmp&0xCC00UL)
+		return -EINVAL;
+	ssc_num=(u8)((tmp&0x000000F0UL)>>4);
+	ssc_value=(u8)(tmp&0x0000000F);
+	if((ssc_num > 7) || (ssc_value > 7))
+		return -EINVAL;
+	mutex_lock(&dev_data->lock);
+	dev_data->ssc1[ssc_num] = ssc_value;
+	// Yep, this switch is ugly, but it's simple
+	switch(ssc_num) {
+		case 0:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_0_20), ssc_value);
+			break;
+		case 1:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_1_20), ssc_value);
+			break;
+		case 2:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_2_2), (ssc_value&0x04)>>2);
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_2_10), ssc_value&0x03);
+			break;
+		case 3:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_3_20), ssc_value);
+			break;
+		case 4:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_4_20), ssc_value);
+			break;
+		case 5:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_5_21), (ssc_value&0x06)>>1);
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_5_0), ssc_value&0x01);
+			break;
+		case 6:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_6_20), ssc_value);
+			break;
+		case 7:
+			cdce913_bf_ins(client, CDCE913_RPARAMS(SSC1_7_20), ssc_value);
+			break;
+		default:
+			break;
+	}
+	mutex_unlock(&dev_data->lock);
+	return count;
+}
+
+
 static DEVICE_ATTR(pdiv, S_IWUSR|S_IRUSR, cdce913_show_pdiv, cdce913_store_pdiv);
 static DEVICE_ATTR(y1, S_IWUSR|S_IRUSR, cdce913_show_y1, cdce913_store_y1);
 static DEVICE_ATTR(y2y3, S_IWUSR|S_IRUSR, cdce913_show_y2y3, cdce913_store_y2y3);
 static DEVICE_ATTR(fs1, S_IWUSR|S_IRUSR, cdce913_show_fs1, cdce913_store_fs1);
 static DEVICE_ATTR(pll1_0, S_IWUSR|S_IRUSR, cdce913_show_pll1_0, cdce913_store_pll1_0);
 static DEVICE_ATTR(pll1_1, S_IWUSR|S_IRUSR, cdce913_show_pll1_1, cdce913_store_pll1_1);
-/*
 static DEVICE_ATTR(ssc1, S_IWUSR|S_IRUSR, cdce913_show_ssc1, cdce913_store_ssc1);
-*/
+
 static struct attribute *cdce913_attributes[] = {
 	&dev_attr_pdiv.attr,
 	&dev_attr_y1.attr,
@@ -296,6 +370,7 @@ static struct attribute *cdce913_attributes[] = {
 	&dev_attr_fs1.attr,
 	&dev_attr_pll1_0.attr,
 	&dev_attr_pll1_1.attr,
+	&dev_attr_ssc1.attr,
 	NULL
 };
 
